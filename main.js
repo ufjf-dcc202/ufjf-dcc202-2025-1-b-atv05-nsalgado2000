934ee268ae5201582444b89b7148c65639d301da
Nicolas Salgado <2000nsalgado@gmail.com>
2025-07-30 16:55:57 -0300

    feat: cria tabuleiro e disco, cria estilo de tabuleiro e estilo de disco

---
+++ main.js
@@ -0,0 +1,30 @@
+const eH1 = document.querySelector("h1");
+eH1.textContent = "Olá, Mundo!";
+
+const eTabuleiro = criaTabuleiro();
+document.body.append(eTabuleiro);
+
+for (let i = 0; i < 7; i++) {
+  const eDisco = criaDisco();
+  eTabuleiro.append(eDisco);
+  if (i < 3) {
+    eDisco.dataset.cor = "branco";
+  }
+  if (i == 3) {
+    eDisco.dataset.cor = "transparente";
+  } else {
+    eDisco.dataset.cor = "preto";
+  }
+}
+
+function criaDisco() {
+  const novoDisco = document.createElement("div");
+  novoDisco.classList.add("disco");
+  return novoDisco;
+}
+
+function criaTabuleiro() {
+  const novoTabuleiro = document.createElement("div");
+  novoTabuleiro.classList.add("tabuleiro");
+  return novoTabuleiro;
+}
